e2b5f8ed439b701a452f518988cb27db47cb2924
polehalieutique <jerome.guitton@agrocampus-ouest.fr>
2023-06-05 17:35:52 +0200

    V0.1.2 / Change calculation : use of only last available asessment data

--- R/system.2.R
+++ R/system.2.R
@@ -7,6 +7,8 @@
 #'
 system.2 <- function(sci_name=NULL,area=NULL,stockdef=NULL,limits=NULL,fishdata=NULL) {
 
+an_6<-2018 # We take into account time series from 2023 to 2017 (6 year)
+
 if (is.null(stockdef) && is.null(limits) && is.null(fishdata))
 {
     stockdef<-stockdef.other()
@@ -18,16 +20,17 @@ if (is.null(stockdef) && is.null(limits) && is.null(fishdata))
   sf::sf_use_s2(FALSE)
 
 
-  fishdata %>% group_by(fishstock,evaluationyear) %>% summarize(year=max(year)) %>%
-    filter(evaluationyear>=2017) ->last.ts.year
+  fishdata %>% filter(year>=an_6) %>% group_by(fishstock) %>%
+    summarize(evaluationyear=max(evaluationyear)) ->last.Eval.year
 
   stockdef %>% right_join(
-    fishdata %>% inner_join(last.ts.year) %>% inner_join (limits) %>%
+    fishdata %>% filter(year>=an_6) %>% inner_join(last.Eval.year) %>%
+      inner_join (limits) %>%
       mutate(f_fmsy=meanf/fmsy,b_bmsy=tbiomass/msybtrigger) %>%
       group_by(fishstock) %>% summarize(mean.f_fmsy=mean(f_fmsy,na.rm=TRUE),mean.b_bmsy=mean(b_bmsy,na.rm=TRUE),road.1=mean.b_bmsy<0.8,
               road.2=mean.b_bmsy>=0.8 && !is.na(mean.f_fmsy),
               road.3=mean.b_bmsy>=0.8 && is.na(mean.f_fmsy),
-              road.4=is.na(mean.b_bmsy) && !is.na(mean.f_fmsy) ,nb.eval=n(),eval.year=paste(evaluationyear,collapse='/'),
+              road.4=is.na(mean.b_bmsy) && !is.na(mean.f_fmsy) ,nb.eval=n(),eval.year=mean(evaluationyear,na.rm=TRUE),
               f_fmsy=paste(f_fmsy,collapse='/'),b_bmsy=paste(b_bmsy,collapse='/'))) %>%
     mutate(roadall=as.numeric(coalesce(road.1,0))+as.numeric(coalesce(road.2,0))+as.numeric(coalesce(road.3,0))+as.numeric(coalesce(road.4,0))) %>%
     filter(roadall!=0)  ->system2.dta

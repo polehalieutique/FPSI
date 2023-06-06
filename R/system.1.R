3a08d012de41797cdc1a2d47fa0ed951087de1d1
polehalieutique <jerome.guitton@agrocampus-ouest.fr>
2023-06-06 14:40:17 +0200

    Use of textual link between iucn and fao area instead of st_intersect

--- R/system.1.R
+++ R/system.1.R
@@ -10,16 +10,21 @@ system.1 <- function(sci_name=NULL,area=NULL,longitude=NULL,latitude=NULL) {
 data(iucn.dta)
 data(sensitive.dta)
 data(area.dta)
+data(iucn_to_stock_area)
 
-  sf::sf_use_s2(FALSE)
+sf::sf_use_s2(FALSE)
 
+#Method with intersection / a little bit longer than using iucn_to_stock_area link table (for each gid from iucn you have the list og fao area concerned (in intersection with))
+#  filter(area.dta,grepl(toupper(area), sub_division_fao)) %>% st_union() %>%
+#    st_as_sf() %>% st_join(filter(iucn.dta,scientific_name==sci_name)) %>%
+#    filter(!is.na(category)) %>% distinct()-> system1.road.5
 
-  filter(area.dta,grepl(toupper(area), sub_division_fao)) %>% st_union() %>%
-    st_as_sf() %>% st_join(filter(iucn.dta,scientific_name==sci_name)) %>%
-    filter(!is.na(category)) %>% distinct()-> system1.road.5
+  iucn.dta %>%
+    filter(scientific_name==sci_name) %>%
+    inner_join(dplyr::filter(iucn_to_stock_area,grepl(toupper(area),sub_division_fao))) %>%
+    filter(!is.na(category)) %>% distinct(id_no,scientific_name,yrcompiled,freshwater,category) %>%
+    mutate(area.req=area)-> system1.road.5
 
-  g1<-ggplot(system1.road.5)+geom_sf(aes())+ggtitle("Lien avec les zones IUCN")
-  print(g1)
 
   nlignes<-dim(system1.road.5)[1]
   if (nlignes==0) {

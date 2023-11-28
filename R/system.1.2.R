e9563d06ff928be70cb54ad16709b81a2b596f8a
polehalieutique <jerome.guitton@agrocampus-ouest.fr>
2023-11-28 15:03:03 +0100

    New IUCN data

--- R/system.1.2.R
+++ R/system.1.2.R
@@ -5,16 +5,19 @@
 #' all<-system.1.2(sci_name='SQUATINA SQUATINA',area='27.')
 #' @export
 #'
+
 system.1.2 <- function(sci_name=NULL,area=NULL,stockdef=NULL,limits=NULL,fishdata=NULL,iucn.dta=NULL,sensitive.dta=NULL,area.dta=NULL,iucn_to_stock_area=NULL,iucn.token=NULL) {
 
 print(paste('First Step for ',sci_name,' in ',area,'for system2 ',sep=''))
-system.2.road1.2.3.4.5<-system.2(sci_name,area,stockdef,limits,fishdata) %>% mutate(method="system2")
+
+system.2.road1.2.3.4.5<-system.2(sci_name,area,stockdef,limits,fishdata)
 
 mix.systems<-length(unique(system.2.road1.2.3.4.5$roadall))==2
 if (dim(system.2.road1.2.3.4.5)[1]==0 | sum(as.numeric(system.2.road1.2.3.4.5$roadall))==0 |mix.systems)
 {
   print(paste('No answer for system2 or mixed system 1 and 2, Second step for System 1',sep=''))
-if (is.null(iucn.token)){  system.1.road.5.6<-system.1(sci_name,area,iucn.dta,sensitive.dta,area.dta,iucn_to_stock_area) %>% mutate(method='system1')
+if (is.null(iucn.token)){
+  system.1.road.5.6<-system.1(sci_name,area,iucn.dta,sensitive.dta,area.dta,iucn_to_stock_area) %>% mutate(method='system1')
 } else
 {
   system.1.road.5.6<-system.1.iucnws(sci_name,area,iucn.dta,sensitive.dta,area.dta,iucn_to_stock_area,token=iucn.token) %>% mutate(method='system1')
@@ -27,22 +30,21 @@ if (is.null(iucn.token)){  system.1.road.5.6<-system.1(sci_name,area,iucn.dta,se
   else {
     if (mix.systems)
      {
-      tmp<-system.2.road1.2.3.4.5 %>% filter(roadall==1) %>% mutate(category=NA,freshwater_system=NA,area.req=NA,ass=NA,source_code=NA,Sensitivity_indicator=NA,Indicator_source=NA) %>%
+      tmp<-system.2.road1.2.3.4.5 %>% mutate(method="system2") %>% filter(roadall==1) %>% mutate(category=NA,freshwater_system=NA,area.req=NA,ass=NA,source_code=NA,Sensitivity_indicator=NA,Indicator_source=NA) %>%
         st_drop_geometry()
 
       tmp.system1<-system.2.road1.2.3.4.5 %>% filter(roadall==0) %>% st_drop_geometry()%>%
-        select(-method) %>%
         inner_join(select(system.1.road.5.6,-fishstock),by=c('scientific_name'))
 
       results<-tmp %>% dplyr::bind_rows(tmp.system1)
       return(results)
      }
-    else( return(system.1.road.5.6))
+    else( return(system.1.road.5.6 ))
     }
 }
 else
 {
-  tmp<-system.2.road1.2.3.4.5
+  tmp<-system.2.road1.2.3.4.5%>% mutate(method="system2")
   return(tmp)
 }
 
--- R/system.1.R
+++ R/system.1.R
@@ -2,40 +2,38 @@
 #' @param scientific_name taxonomic name of the product
 #' @param area catches area mentionned for the product
 #' @examples
-#' pre.score.2<-system.2(sci_name='SQUATINA SQUATINA',area='27.')
+#' pre.score.2<-system.(sci_name='SQUATINA SQUATINA',area='27.')
 #' @export
 #'
 system.1 <- function(sci_name=NULL,area=NULL,iucn.dta=NULL,sensitive.dta=NULL,area.dta=NULL,iucn_to_stock_area=NULL,longitude=NULL,latitude=NULL) {
 
 if (!is.null(iucn.dta)) {data(iucn.dta)}
 if (!is.null(sensitive.dta)) {data(sensitive.dta)}
-if (!is.null(area.dta)) {data(area.dta)}
-if (!is.null(iucn_to_stock_area)) {data(iucn_to_stock_area)}
 
 sf::sf_use_s2(FALSE)
 
-#Method with intersection / a little bit longer than using iucn_to_stock_area link table (for each gid from iucn you have the list og fao area concerned (in intersection with))
-#  filter(area.dta,grepl(toupper(area), sub_division_fao)) %>% st_union() %>%
-#    st_as_sf() %>% st_join(filter(iucn.dta,scientific_name==sci_name)) %>%
-#    filter(!is.na(category)) %>% distinct()-> system1.road.5
 
-  iucn.dta %>%
-    filter(scientific_name==sci_name) %>%
-    inner_join(dplyr::filter(iucn_to_stock_area,grepl(toupper(area),sub_division_fao))) %>%
-    filter(!is.na(category)) %>% distinct(id_no,scientific_name,yrcompiled,freshwater,category) %>%
-    mutate(area.req=area)-> system1.road.5
-
-
-  nlignes<-dim(system1.road.5)[1]
 
   area.req.sens<-'Global'
-  if (substr(area,1,2) %in% c('37','27','SA')) {
+  area.req.sens.iucn<-'Global'
+    if (substr(area,1,2) %in% c('SA','37')) {area.req.sens.iucn<-'Mediterranean'
+    area.req.sens<-'Mediterranean'}
+  if (substr(area,1,2) %in% c('27')) {area.req.sens.iucn<-'Europe'
+  area.req.sens<-'27'}
 
-    if (substr(area,1,2)=='SA') {area.req.sens<-'37'} else {area.req.sens<-substr(area,1,2)}
-  }
+  iucn.dta %>%
+    mutate(scientific_name=scientificName) %>%
+    filter(scientific_name==sci_name,scopes==area.req.sens.iucn) %>%
+    filter(!is.na(category)) %>%
+  mutate(area.req=area)-> system1.road.6
+
+nlignes<-dim(system1.road.6)[1]
+
 
 
   if (nlignes==0) {
+
+
     result.local<-data.frame(Sensitivity_indicator=NULL)
     result.local<-sensitive.dta %>%  dplyr::filter(scientific_name==sci_name & area.req==area.req.sens)%>%
       dplyr::mutate(id_no=NA,yrcompiled=NA,freshwater=NA,category=NA) %>%
@@ -50,8 +48,8 @@ sf::sf_use_s2(FALSE)
 
   }else
   {
-    result.local<-system1.road.5 %>% mutate(area.req=area.req.sens) %>% left_join(sensitive.dta)
-    result.global<-system1.road.5 %>% mutate(area.req='Global') %>%  st_drop_geometry() %>%
+    result.local<-system1.road.6 %>% mutate(area.req=area.req.sens) %>% left_join(sensitive.dta)
+    result.global<-system1.road.6 %>% mutate(area.req='Global') %>%
       left_join(sensitive.dta)
     if (is.na(result.local$Sensitivity_indicator)) {results<-result.global} else {results<-result.local}
   }
